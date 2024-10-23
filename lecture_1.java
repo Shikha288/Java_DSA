dabeb395ab7d23bc7e17ed285dd66eaefad97066
Shikha288 <shikhaanand288@gmail.com>
2024-10-23 21:38:55 +0530

    Array Declaration.

---
+++ lecture_1.java
@@ -0,0 +1,51 @@
+
+
+    /*
+     Ques2: calculate maximum
+     Input={1,5,3}
+     Output=5
+     */
+   /* 
+    public static void maximunOfarray(){
+        int[] arr={1,5,3};
+        int max=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(max<arr[i]){
+                max=arr[i];
+            }   
+        }
+        System.out.println("Maximum element of the array is:"+max);
+
+    }
+    public static void main(String[] args) {
+        maximunOfarray();
+    }
+}
+    */
+/*
+ Ques3: Search the givem X in array
+ if present then return the index else return -1
+ Input: {1,5,3}  X=5
+ Output=1
+ */
+/* 
+ public static void indexOfElement(){
+    int[] arr= {1,5,3};
+    int x=5;
+    int index=-1;
+    for (int i = 0; i < arr.length; i++) {
+        if(arr[i]==x){
+           index = i;
+        }
+        else{
+            System.out.println("-1");
+        }  
+    }
+    System.out.println("Index of the element is:"+index);
+
+ }
+ public static void main(String[] args) {
+    indexOfElement();
+ }
+}
+ */
